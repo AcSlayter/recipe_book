e1e7afc31bc181a37ab9b9115504e77428579298
Aaron <aaronbcain@gmial.com>
2016-11-19 00:31:42 -0800

    adding mongoose interaction

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,35 @@
 var express = require('express')
 var app = express()
 
+// Load mongoose package
+var mongoose = require('mongoose');
+// Connect to MongoDB and create/use database called todoAppTest
+mongoose.connect('mongodb://192.168.1.155/app');
+// Create a schema
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+});
+// Create a model based on the schema
+var Todo = mongoose.model('Todo', TodoSchema);
+
+// Create a todo in memory
+var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
+// Save it to database
+todo.save(function(err){
+  if(err)
+    console.log(err);
+  else
+    console.log(todo);
+});
+
+Todo.find(function (err, todos) {
+  if (err) return console.error(err);
+  console.log(todos)
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
