f3d28459529cd2873bf70d37194abff86d70a920
Aaron <aaronbcain@gmial.com>
2016-11-20 20:11:20 -0800

    Adding static moving mongo to function

--- myapp/app.js
+++ myapp/app.js
@@ -1,38 +1,42 @@
 var express = require('express')
 var app = express()
 
-// Load mongoose package
-var mongoose = require('mongoose');
-// Connect to MongoDB and create/use database called todoAppTest
-mongoose.connect('mongodb://192.168.1.155/app');
-// Create a schema
-var TodoSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
-  note: String,
-  updated_at: { type: Date, default: Date.now },
-});
-// Create a model based on the schema
-var Todo = mongoose.model('Todo', TodoSchema);
 
-// Create a todo in memory
-var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
-// Save it to database
-todo.save(function(err){
-  if(err)
-    console.log(err);
-  else
-    console.log(todo);
-});
+function db_connection () {
+        // Load mongoose package
+        var mongoose = require('mongoose');
+        // Connect to MongoDB and create/use database called todoAppTest
+        mongoose.connect('mongodb://192.168.1.155/app');
+        // Create a schema
+        var TodoSchema = new mongoose.Schema({
+          name: String,
+          completed: Boolean,
+          note: String,
+          updated_at: { type: Date, default: Date.now },
+        });
+        // Create a model based on the schema
+        var Todo = mongoose.model('Todo', TodoSchema);
 
-Todo.find(function (err, todos) {
-  if (err) return console.error(err);
-  console.log(todos)
-});
+        // Create a todo in memory
+        var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
+        // Save it to database
+        todo.save(function(err){
+          if(err)
+            console.log(err);
+          else
+            console.log(todo);
+        });
+        Todo.find(function (err, todos) {
+          if (err) return console.error(err);
+          console.log(todos)
+        });
+}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/', express.static('static'));
+
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
